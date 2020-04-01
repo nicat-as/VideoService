168c01d591ad324cff8d3326338820bfb655f9c1
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2020-04-01 22:21:22 +0400

    Adding StorageService to upload file to directory.Changing ApiController.upload method

--- src/main/java/com/uniso/video/controller/ApiController.java
+++ src/main/java/com/uniso/video/controller/ApiController.java
@@ -3,12 +3,17 @@ package com.uniso.video.controller;
 import com.uniso.video.sdk.Client;
 import com.uniso.video.sdk.ClientFactory;
 import com.uniso.video.sdk.domain.video.Video;
+import com.uniso.video.sdk.domain.video.VideoInput;
+import com.uniso.video.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 
@@ -16,15 +21,21 @@ import java.io.File;
 @RequestMapping("api")
 public class ApiController {
 
+    @Value("${api.video.sandbox}")
+    private String sandboxKey;
+
+    @Autowired
+    private StorageService storageService;
+
     @PostMapping("/upload")
-    public ResponseEntity<?> upload(@RequestParam(name = "path") String path){
+    public ResponseEntity<?> upload(@RequestParam(name = "file") MultipartFile file){
         Video video = null;
+        String filePath;
         try {
-            Client client = new ClientFactory().createSandbox("Xc1YmjmUzqNkTtdXQDtVyDQeB5sF1fPtoit5scz5pWw");
-            if (path.isEmpty()){
-                path="/home/nicat/Documents/aws_con.mp4";
-            }
-            video = client.videos.upload(new File(path));
+            filePath = storageService.store(file);
+            Client client = new ClientFactory().createSandbox(sandboxKey);
+            video = client.videos.upload(new File(filePath));
+            storageService.deleteFile(filePath);
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
