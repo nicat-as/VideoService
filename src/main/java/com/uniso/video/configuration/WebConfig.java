7e6117f748036afb79c047fce7c5782e86d8d7d1
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2020-04-08 22:15:36 +0400

    Adding cors

--- src/main/java/com/uniso/video/configuration/WebConfig.java
+++ src/main/java/com/uniso/video/configuration/WebConfig.java
@@ -3,6 +3,7 @@ package com.uniso.video.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON)
         .ignoreAcceptHeader(true);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
