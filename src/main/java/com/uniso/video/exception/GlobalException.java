0c4c5a5ccaf52f8e310785a1020d671545637a61
Nicat Asgerzade <nicat.asgerzade.9889@gmail.com>
2020-04-12 16:05:34 +0400

    Modifying GlobalException and pom.xml

--- src/main/java/com/uniso/video/exception/GlobalException.java
+++ src/main/java/com/uniso/video/exception/GlobalException.java
@@ -5,19 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalException {
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<?> globalExceptionHandler(){
+    public final ResponseEntity<?> globalExceptionHandler(Exception e) {
         ApiResponse<String> apiResponse = new ApiResponse<>();
 
         apiResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         apiResponse.setErrorCode(-1);
-        apiResponse.setErrorMessage("Something went wrong!");
+        apiResponse.setErrorMessage(e.getMessage());
 
-        return new ResponseEntity<>(apiResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
